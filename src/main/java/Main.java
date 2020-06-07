1e46d147f323142534e727777c8fd140ed6252a9
Adam <adamir24@gmail.com>
2020-06-07 22:09:00 +0200

    Second Initial Commit - without garbages.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,12 +10,6 @@ public class Main {
     public static void main (String[] args) {
         JOptionPane.showMessageDialog(null, "Sef nie ksycy");
 
-        Produkt produkt = new Produkt();
-        produkt.setNazwaProdkutu("Jabadaba");
-        produkt.setIloscProduktu(10);
-
-        System.out.println(produkt.getNazwaProdkutu() + " " + produkt.getIloscProduktu());
-
         ProduktModel produktModel = new ProduktModel();
         produktModel.dodajProdukt("Drukarka", 1);
     }
